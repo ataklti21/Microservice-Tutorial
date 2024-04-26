34fa3c69e8ddeb5380ac991b068e9e3839c74435
ataklti21 <atuka2@gmail.com>
2024-04-26 09:36:25 -0400

    Notification Module Added

--- notificationmodule/src/main/java/com/atuka/notification/NotificationApplication.java
+++ notificationmodule/src/main/java/com/atuka/notification/NotificationApplication.java
@@ -1,11 +1,11 @@
 package com.atuka.notification;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
 public class NotificationApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class, args);
