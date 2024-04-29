c1618a07d19e9957a2e1fe4cea0b3a27309d3b9e
ataklti21 <atuka2@gmail.com>
2024-04-29 01:26:22 -0400

    RabbitMqconfig

---
+++ amqp/src/main/java/com/atuka/amqp/RabbitMqconfig.java
@@ -0,0 +1,46 @@
+package com.atuka.amqp;
+
+
+import lombok.AllArgsConstructor;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+@AllArgsConstructor
+@EnableAutoConfiguration
+public class RabbitMqconfig {
+
+    private final ConnectionFactory connectionFactory;
+
+    /**
+     * this allows as to send message to a queue
+     * @return
+     */
+    @Bean
+    public AmqpTemplate amqpTemplate() {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(messageConverter());
+        return rabbitTemplate;
+    }
+
+    @Bean
+    public SimpleRabbitListenerContainerFactory simpleRoutingConnectionFactory() {
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setMessageConverter(messageConverter());
+        return factory;
+    }
+
+    @Bean
+    public MessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+}
